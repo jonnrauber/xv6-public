a9f975f3f56d8acb25c2c09da37fc25cb7d3cde2
jonathan.rauber <jonathan.rauber@uffs.edu.br>
2017-10-18 10:18:16 -0200

    testes

--- teste3.c
+++ teste3.c
@@ -7,12 +7,10 @@
 void loop(int p){
 	double i, j;
 
-	printf(1, "\np%d -- Comecei\n", p);
-
-	for(i = 0; i < 100000; i++)
+	for(i = 0; i < 10000; i++)
 		for(j=0; j < 10000; j++);
 
-	printf(1, "\np%d -- Terminei\n", p);
+	printf(1, "%d\n", p);
 }
 
 int main(void){
--- teste4.c
+++ teste4.c
@@ -7,43 +7,41 @@
 void loop(int p){
 	double i, j;
 
-	printf(1, "\np%d -- Comecei\n", p);
-
-	for(i = 0; i < 100000; i++)
+	for(i = 0; i < 10000; i++)
 		for(j=0; j < 10000; j++);
 
-	printf(1, "\np%d -- Terminei\n", p);
+	printf(1, "p%d\n", p);
 }
 
 int main(void){
 	int p[5];
 
 
-	p[0] = fork(50);
+	p[0] = fork(10);
 	if(p[0] == 0){
 		loop(0);
 		exit();
 	}
 		
-	p[1] = fork(25);
+	p[1] = fork(20);
 	if(p[1] == 0){
 		loop(1);
 		exit();
 	}
 		
-	p[2] = fork(75);
+	p[2] = fork(30);
 	if(p[2] == 0){
 		loop(2);
 		exit();
 	}	
 	
-	p[3] = fork(100);
+	p[3] = fork(40);
 	if(p[3] == 0){
 		loop(3);
 		exit();	
 	}
 
-	p[4] = fork(5);
+	p[4] = fork(50);
 	if(p[4] == 0){
 		loop(4);
 		exit();
